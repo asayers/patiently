1cd24608b9a21a586b120fac5e0eed1245c0eafb
Alex Sayers <alex@asayers.com>
2024-11-07 16:00:53 +0900

    Simplify the queue dir
    * When a job finishes (successfully or not), its queue file is deleted
    * The status of a job is now a free-form string written into the queue file
    * The filename of the queue file is now simply the job ID
    
    This new structure simplifies the inotify code, because now we _only_
    have to care about deletions.  This code has also been ported to use
    rustix.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use anyhow::{bail, Context};
+use anyhow::{bail, ensure, Context};
 use bpaf::Bpaf;
-use enum_map::{enum_map, Enum};
-use inotify::{EventMask, Inotify, WatchMask};
-use std::fmt;
+use rustix::fs::inotify;
+use rustix::io::Errno;
+use std::collections::BTreeMap;
 use std::fs::File;
+use std::io::{Seek, Write};
+use std::mem::MaybeUninit;
+use std::os::fd::AsFd;
 use std::path::{Path, PathBuf};
-use std::process::{self, Command};
-use std::str::FromStr;
+use std::process::{Command, ExitCode};
 use tracing::*;
 
 #[derive(Bpaf)]
@@ -20,15 +22,9 @@ struct Opts {
     args: Vec<String>,
 }
 
-fn main() {
-    if let Err(e) = main_2(opts().run()) {
-        let es = e.chain().map(|x| x.to_string()).collect::<Vec<_>>();
-        error!("{}", es.join(": "));
-        process::exit(1);
-    }
-}
+fn main() -> anyhow::Result<ExitCode> {
+    let opts = opts().run();
 
-fn main_2(opts: Opts) -> anyhow::Result<()> {
     let level_filter = match std::env::var("RUST_LOG") {
         Ok(s) => s.parse()?,
         Err(_) => tracing_subscriber::filter::LevelFilter::WARN,
@@ -39,49 +35,38 @@ fn main_2(opts: Opts) -> anyhow::Result<()> {
         .without_time()
         .init();
 
-    let qdir = std::env::var("QUEUE_DIR").map_or(PathBuf::from(".patiently"), PathBuf::from);
-    std::fs::create_dir_all(&qdir)?;
+    let qdir = QueueDir::open()?;
 
     match opts.cmd {
-        None => status(&qdir)?,
-        Some(cmd) => {
-            let mut state = State::new(qdir)?;
-            let res = info_span!("", id = state.id)
-                .in_scope(|| run_job(&mut state, cmd, opts.args, opts.jobs))
-                .context(state.id);
-            if let Err(e) = res {
-                // Make an attempt to mark the job as crashed, ignoring new errors
-                let _ = state.change_status(Status::Crashed);
-                return Err(e);
-            }
+        None => {
+            status(&qdir)?;
+            Ok(ExitCode::SUCCESS)
         }
+        Some(cmd) => State::new(qdir)?.run(cmd, opts.args, opts.jobs),
     }
-    Ok(())
 }
 
-fn status(qdir: &Path) -> anyhow::Result<()> {
+fn status(qdir: &QueueDir) -> anyhow::Result<()> {
     use std::fmt::Write;
     let mut tp = liveterm::TermPrinter::new(std::io::stdout());
     loop {
-        let jobs = list_jobs(&qdir)?;
-        let mut totals = enum_map! { _ => 0 };
-        let mut n_unfinished = 0;
-        for (_, status) in &jobs {
-            // println!("{id}: {status}");
-            totals[*status] += 1;
-            if !status.is_finished() {
-                n_unfinished += 1;
+        let jobs = qdir.list()?;
+        let mut totals = BTreeMap::<String, usize>::default();
+        for &id in &jobs {
+            match qdir.get_status(id) {
+                Ok(status) => *totals.entry(status).or_default() += 1,
+                Err(e) => warn!("{}: {e}", id.0),
             }
         }
 
         tp.clear()?;
         tp.buf.clear();
-        for (status, count) in totals {
+        for (status, count) in &totals {
             writeln!(tp.buf, "{:>10}: {count}", status.to_string())?;
         }
         tp.print()?;
 
-        if n_unfinished == 0 {
+        if totals.values().sum::<usize>() == 0 {
             break;
         }
 
@@ -90,230 +75,179 @@ fn status(qdir: &Path) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run_job(state: &mut State, cmd: String, args: Vec<String>, jobs: usize) -> anyhow::Result<()> {
-    state
-        .wait_for_precursors(jobs)
-        .context("While waiting for precursors")?;
-
-    state.change_status(Status::Running)?;
-    let exit_code = Command::new(cmd).args(args).status()?;
-
-    let final_status = if exit_code.success() {
-        Status::Finished
-    } else {
-        Status::Failed
-    };
-    state.change_status(final_status)?;
-    Ok(())
-}
-
 struct State {
-    qdir: PathBuf,
-    id: usize,
-    status: Status,
-    // Reflects the status at the time new() was called.  May be stale.
-    precursors: Vec<(usize, Status)>,
+    qdir: QueueDir,
+    our_id: JobId,
+    file: File,
+    ahead_of_us: Vec<JobId>,
 }
 
 impl State {
-    fn new(qdir: PathBuf) -> anyhow::Result<State> {
-        let (id, precursors) = loop {
-            let (id, precursors) = get_precusors(&qdir).context("Get precursors")?;
-            // Try to create the queue file
-            let path = qdir.join(format!("patiently.{id}.{}", Status::Waiting));
-            let res = File::options().create_new(true).append(true).open(path);
-            if res.is_ok() {
-                // We claimed this name
-                break (id, precursors);
+    fn new(qdir: QueueDir) -> anyhow::Result<State> {
+        let mut ahead_of_us;
+        let mut our_id;
+        let file = loop {
+            ahead_of_us = qdir.list().context("List existing jobs")?;
+            // Our ID is going to be one more than the latest existing job
+            our_id = JobId(ahead_of_us.iter().map(|x| x.0).max().map_or(0, |x| x + 1));
+            // Try to create a job file
+            match qdir.try_create(our_id) {
+                Ok(file) => break file, // We claimed this name
+                Err(_) => continue,     // Someone else got there first.  Retry
             }
-            // Someone else got there first.  Retry
         };
-        // TODO: Check precursor flocks, set to "cancelled" if missing
-        Ok(State {
+        let _g = info_span!("", job_id = our_id.0).entered();
+        info!("Claimed an ID");
+        let mut state = State {
             qdir,
-            id,
-            precursors,
-            status: Status::Waiting,
-        })
+            ahead_of_us,
+            our_id,
+            file,
+        };
+        state.set_status("waiting")?;
+        Ok(state)
+    }
+}
+
+impl Drop for State {
+    fn drop(&mut self) {
+        let _ = self.qdir.remove(self.our_id);
+    }
+}
+
+impl State {
+    fn run(mut self, cmd: String, args: Vec<String>, jobs: usize) -> anyhow::Result<ExitCode> {
+        let _g = info_span!("", job_id = self.our_id.0).entered();
+        self.wait(jobs).context("While waiting for other jobs")?;
+        self.set_status("running")?;
+        let status = Command::new(cmd).args(args).status()?;
+        let code = match status.code() {
+            Some(x) => x as u8,
+            None => 1, // stopped by a signal
+        };
+        Ok(ExitCode::from(code))
     }
 
-    fn change_status(&mut self, to: Status) -> anyhow::Result<()> {
-        let from = self.status;
-        std::fs::rename(
-            self.qdir.join(format!("patiently.{}.{from}", self.id)),
-            self.qdir.join(format!("patiently.{}.{to}", self.id)),
-        )
-        .context("Changing status")?;
-        info!(%from, %to, "Changing status");
-        self.status = to;
+    fn set_status(&mut self, status: &str) -> std::io::Result<()> {
+        self.file.seek(std::io::SeekFrom::Start(0))?;
+        self.file.write_all(status.as_bytes())?;
         Ok(())
     }
 
-    fn qfile(&self) -> PathBuf {
-        self.qdir
-            .join(format!("patiently.{}.{}", self.id, self.status))
-    }
+    fn wait(&mut self, max_jobs: usize) -> anyhow::Result<()> {
+        if self.ahead_of_us.len() < max_jobs {
+            // Just a fast-path to avoid setting up inotify and then immediately
+            // dropping it
+            return Ok(());
+        }
 
-    fn wait_for_precursors(&mut self, max_jobs: usize) -> anyhow::Result<()> {
-        info!("Waiting for {} jobs to finish", self.precursors.len());
-        let mut inotify = loop {
-            match Inotify::init() {
-                Ok(x) => break x,
-                Err(_) => std::thread::sleep(std::time::Duration::from_secs(1)),
-            }
+        let ino_fd = inotify::init(inotify::CreateFlags::empty())?;
+        let our_wd = match inotify::add_watch(
+            &ino_fd,
+            &self.qdir.file_for(self.our_id),
+            inotify::WatchFlags::DELETE_SELF | inotify::WatchFlags::MOVE_SELF,
+        ) {
+            Ok(wd) => wd,
+            Err(_) => bail!("Our own file was removed; cancelling"),
         };
 
-        let mut inotify_buf = vec![0; 1024];
-        let our_wd =
-            match inotify.add_watch(&self.qfile(), WatchMask::DELETE_SELF | WatchMask::MOVE_SELF) {
-                Ok(x) => x,
-                Err(_) => {
-                    // Our output file has already been deleted
-                    warn!("Output file removed, exiting");
-                    process::exit(0);
-                }
-            };
-        let mut watches = std::collections::HashMap::new();
+        let mut ino_buf = [const { MaybeUninit::uninit() }; 1024];
+        let mut evs = inotify::Reader::new(&ino_fd, &mut ino_buf);
+        let mut watches = std::collections::HashSet::new();
 
         loop {
+            info!(
+                "Waiting for {} jobs to finish",
+                self.ahead_of_us.len() + watches.len()
+            );
             while watches.len() < max_jobs {
-                match self.precursors.pop() {
-                    Some((x, _)) => {
-                        match inotify.add_watch(
-                            &self.qdir.join(format!("patiently.{x}.{}", Status::Waiting)),
-                            WatchMask::DELETE_SELF | WatchMask::MOVE_SELF,
+                match self.ahead_of_us.pop() {
+                    Some(job) => {
+                        match inotify::add_watch(
+                            &ino_fd,
+                            &self.qdir.file_for(job),
+                            inotify::WatchFlags::DELETE_SELF,
                         ) {
                             Ok(wd) => {
-                                watches.insert(wd, x);
+                                watches.insert(wd);
                             }
-                            Err(_) => {
-                                if let Ok(wd) = inotify.add_watch(
-                                    &self.qdir.join(format!("patiently.{x}.{}", Status::Running)),
-                                    WatchMask::DELETE_SELF | WatchMask::MOVE_SELF,
-                                ) {
-                                    watches.insert(wd, x);
-                                } else {
-                                    // I guess it finished already?
-                                }
-                            }
+                            Err(Errno::NOENT) => info!("{} is already gone", job.0),
+                            Err(e) => warn!("Couldn't install a watch on {}: {e}", job.0),
                         }
                     }
                     None => return Ok(()),
                 }
             }
-            for ev in inotify
-                .read_events_blocking(&mut inotify_buf)
-                .context("Getting events")?
-            {
-                if ev.wd == our_wd {
-                    warn!("Output file removed, exiting");
-                    process::exit(0);
-                }
-                match ev.mask {
-                    EventMask::IGNORED | EventMask::DELETE_SELF => {
-                        watches.remove(&ev.wd);
-                        continue;
-                    }
-                    EventMask::MOVE_SELF => {
-                        let x = match watches.get(&ev.wd) {
-                            Some(x) => x,
-                            None => bail!("{:?}: Couldn't find watch", ev),
-                        };
-                        let exists =
-                            |status| self.qdir.join(format!("patiently.{x}.{status}")).exists();
-                        if exists(Status::Running) {
-                            // The file just switched to "running" status.
-                            // Keep watching it.
-                        } else if exists(Status::Finished)
-                            || exists(Status::Failed)
-                            || exists(Status::Crashed)
-                        {
-                            // The file just switched to "finished"/"failed"
-                            // status.  Remove the watch.
-                            inotify.rm_watch(ev.wd).context("Removing watch")?;
-                        } else if exists(Status::Waiting) {
-                            bail!("File moved, but status is still waiting?");
-                        } else {
-                            // Someone has renamed the file to something we
-                            // don't recognise.
-                            inotify.rm_watch(ev.wd).context("Removing watch")?;
-                        }
-                    }
-                    mask => bail!("Unexpected event mask {:?}", mask),
-                }
+            info!("Waiting for events on {} file(s)", watches.len());
+            let ev = evs.next().context("Getting events")?;
+            info!("Got event: {ev:?}");
+            ensure!(ev
+                .events()
+                .intersects(inotify::ReadFlags::DELETE_SELF | inotify::ReadFlags::IGNORED));
+            if ev.wd() == our_wd {
+                bail!("Our own file was removed; cancelling");
             }
+            watches.remove(&ev.wd());
         }
     }
 }
 
-fn get_precusors(qdir: &Path) -> anyhow::Result<(usize, Vec<(usize, Status)>)> {
-    let mut precursors = list_jobs(qdir)?;
-    // Increment the ID regardless of the status
-    let next_id = precursors.iter().map(|x| x.0).max().map_or(0, |x| x + 1);
-    // Don't wait for completed jobs
-    precursors.retain(|(_, status)| !status.is_finished());
-    Ok((next_id, precursors))
-}
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct JobId(usize);
 
-fn list_jobs(qdir: &Path) -> anyhow::Result<Vec<(usize, Status)>> {
-    let mut jobs = std::fs::read_dir(qdir)?
-        .filter_map(|x| {
-            let x = x.ok()?;
-            match x.file_type() {
-                Ok(ft) if ft.is_file() => (),
-                _ => return None,
-            }
-            let name = x.file_name();
-            let mut tokens = name.to_str()?.split('.');
-            if tokens.next()? != "patiently" {
-                return None;
-            }
-            let id: usize = tokens.next()?.parse().ok()?;
-            let status: Status = tokens.next()?.parse().ok()?;
-            Some((id, status))
-        })
-        .collect::<Vec<_>>();
-    jobs.sort_unstable_by_key(|x| x.0);
-    Ok(jobs)
-}
+pub struct QueueDir(PathBuf);
 
-#[derive(Copy, Clone, Enum)]
-enum Status {
-    Waiting,
-    Running,
-    Finished,
-    Failed,
-    Crashed,
-}
-impl fmt::Display for Status {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Status::Waiting => f.write_str("waiting"),
-            Status::Running => f.write_str("running"),
-            Status::Finished => f.write_str("finished"),
-            Status::Failed => f.write_str("failed"),
-            Status::Crashed => f.write_str("crashed"),
-        }
+impl QueueDir {
+    fn open() -> std::io::Result<QueueDir> {
+        let qdir = std::env::var("QUEUE_DIR").map_or(PathBuf::from(".patiently"), PathBuf::from);
+        std::fs::create_dir_all(&qdir)?;
+        Ok(QueueDir(qdir))
     }
-}
-impl FromStr for Status {
-    type Err = anyhow::Error;
-    fn from_str(s: &str) -> anyhow::Result<Status> {
-        match s {
-            "waiting" => Ok(Status::Waiting),
-            "running" => Ok(Status::Running),
-            "finished" => Ok(Status::Finished),
-            "failed" => Ok(Status::Failed),
-            "crashed" => Ok(Status::Crashed),
-            _ => bail!("{s}: Unrecognised status"),
+
+    fn list(&self) -> std::io::Result<Vec<JobId>> {
+        let mut jobs = vec![];
+        for dent in std::fs::read_dir(&self.0)? {
+            let dent = dent?;
+            let name = dent.file_name().to_string_lossy().into_owned();
+            if !dent.file_type().is_ok_and(|ft| ft.is_file()) {
+                warn!(
+                    %name, rsn = "not a file",
+                    "Ignoring unexpected entry in queue dir",
+                );
+                continue;
+            }
+            let Some(id) = dent
+                .file_name()
+                .to_str()
+                .and_then(|name| name.parse::<usize>().ok())
+            else {
+                warn!(
+                    %name, rsn = "non-numeric filename",
+                    "Ignoring unexpected entry in queue dir",
+                );
+                continue;
+            };
+            jobs.push(JobId(id));
         }
+        jobs.sort_unstable();
+        Ok(jobs)
     }
-}
-impl Status {
-    fn is_finished(self) -> bool {
-        match self {
-            Status::Waiting | Status::Running => false,
-            Status::Finished | Status::Failed | Status::Crashed => true,
-        }
+
+    fn file_for(&self, id: JobId) -> PathBuf {
+        self.0.join(id.0.to_string())
+    }
+
+    fn try_create(&self, id: JobId) -> std::io::Result<File> {
+        let path = self.0.join(id.0.to_string());
+        let file = File::options().create_new(true).write(true).open(path)?;
+        Ok(file)
+    }
+
+    fn remove(&self, id: JobId) -> std::io::Result<()> {
+        std::fs::remove_file(self.0.join(id.0.to_string()))
+    }
+
+    fn get_status(&self, id: JobId) -> std::io::Result<String> {
+        std::fs::read_to_string(self.0.join(id.0.to_string()))
     }
 }
